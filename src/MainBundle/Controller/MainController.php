60b2a63c51e9fbed2afbe17e5b34be66c1903a8f
Sherkazy <sheki92m@mail.ru>
2017-03-27 12:48:37 +0600

    Result chart

--- src/MainBundle/Controller/MainController.php
+++ src/MainBundle/Controller/MainController.php
@@ -9,12 +9,34 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class MainController extends Controller
 {
-    /**
-     * @Route("/", name="main_index")
-     * @Template()
-     */
-    public function indexAction()
-    {
-        return;
+	/**
+	 * @Route("/", name="main_index")
+	 * @Template()
+	 */
+	public function indexAction()
+	{
+		$test   = $this->getDoctrine()->getRepository('BilimBundle:Test')->findAll();
+		$result = null;
+		return $this->render('MainBundle:Main:index.html.twig', array('test' => $test, 'result' => $result));
+	}
+
+	/**
+	 * @Route("/result", name="result")
+	 * @Template()
+	 */
+	public function resultAction()
+	{
+		$result = $_POST['select'];
+		$test   = $this->getDoctrine()->getRepository('BilimBundle:Test')
+					   ->createQueryBuilder('test')
+					   ->where('test.name=:name')
+					   ->setParameter('name', $result)
+					   ->getQuery()
+					   ->getOneOrNullResult();
+
+		$result = $this->getDoctrine()->getRepository('BilimBundle:Student')->findByTest($test);
+    	$test   = $this->getDoctrine()->getRepository('BilimBundle:Test')->findAll();
+
+        return $this->render('MainBundle:Main:index.html.twig', array('test' => $test, 'result' => $result));
     }
 }
